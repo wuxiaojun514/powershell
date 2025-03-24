6dde483163fc705fc8ba7c91f430208ec170a64f
wuxiaojun514 <wuxiaojun514@163.com>
2025-03-24 15:17:38 +0800

    fix to 4808, use "tostring" instead of "as string" since "as string" will return null

--- src/Commands/Utilities/ListItemHelper.cs
+++ src/Commands/Utilities/ListItemHelper.cs
@@ -166,10 +166,10 @@ namespace PnP.PowerShell.Commands.Utilities
                                     var taxSession = clonedContext.Site.GetTaxonomySession();
                                     TaxonomyItem taxonomyItem = null;
                                     bool updateTaxItemValue = true;
-                                    if (value != null && !Guid.TryParse(value as string, out termGuid))
+                                    if (value != null && !Guid.TryParse(value?.ToString(), out termGuid))
                                     {
                                         // Assume it's a TermPath
-                                        taxonomyItem = clonedContext.Site.GetTaxonomyItemByPath(value as string);
+                                        taxonomyItem = clonedContext.Site.GetTaxonomyItemByPath(value?.ToString());
                                         if (taxonomyItem == null)
                                         {
                                             updateTaxItemValue = false;
